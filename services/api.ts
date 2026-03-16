e0764d3a4302cd66a445e64fab3b952bb0856c9c
Glenn Torrens <glenntorrens@gmail.com>
2026-03-16 19:05:45 +0900

    implemented moviecard, trendingcard, useFetch

--- services/api.ts
+++ services/api.ts
@@ -3,14 +3,13 @@ export const TMDB_CONFIG = {
   API_KEY: process.env.EXPO_PUBLIC_TMDB_API_KEY,
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_API_KEY}`,
   },
 };
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?api_key=${TMDB_CONFIG.API_KEY}&query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?api_key=${TMDB_CONFIG.API_KEY}&sort_by=popularity.desc`;
 
     // the tmdb config object contains the base URL for the TMDB API, the API key retrieved from environment variables, and the necessary headers for making API requests. The fetchMovies function constructs the appropriate endpoint based on whether a search query is provided. If a query is present, it uses the search endpoint; otherwise, it defaults to fetching popular movies. The encodeURIComponent function is used to safely encode the query string, ensuring that any special characters are properly handled in the API request.       
   // the endcode URI component is used to ensure that any special characters in the query string are properly escaped, preventing issues with the API request.
